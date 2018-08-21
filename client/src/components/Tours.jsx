7f4867c1b86936f967e52d4ce519549830b534da
Adam Bohannon <abo46n2@gmail.com>
2018-08-21 16:55:55 -0700

    Added AntD table component, started styling

--- client/src/components/Tours.jsx
+++ client/src/components/Tours.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 import { Card } from 'components/common';
 
 class Tours extends Component {
   render() {
+    const rowSelection = {
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+      },
+      getCheckboxProps: record => ({
+        disabled: record.tour === 'Disabled User', // Column configuration not to be checked
+        tour: record.tour,
+      }),
+    };
+
+    const columns = [
+      {
+        title: 'Tour Name',
+        dataIndex: 'tour',
+      },
+    ]
+
+    const data = [
+      {
+        key: '1',
+        tour: 'Circa Survive Anniversary Tour'
+      },
+      {
+        key: '2',
+        tour: 'Every Time I Die Fuck You Tour'
+      },
+    ]
+
     return (
-      <Card title="Tours">
-        <p>Content</p>
+      <Card title="Current Tours">
+        <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
       </Card>
     );
   }

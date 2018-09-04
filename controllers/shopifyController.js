2f2227f413f9dd220eed0c40dae480424cb907b8
Adam Bohannon <abo46n2@gmail.com>
2018-09-03 20:32:24 -0700

    Created db service

--- controllers/shopifyController.js
+++ controllers/shopifyController.js
@@ -1,5 +1,5 @@
-const Show = require('../models/show');
 const shopify = require('../services/shopify');
+const dbService = require('../services/db-service');
 
 exports.fetchTours = async (req, res) => {
   try {
@@ -19,30 +19,7 @@ exports.fetchAllShows = async (req, res) => {
 
     if (showsList.length < 1) throw new Error('No shows found.');
 
-    // TODO: Break DB query into service
-    // TODO: Also create check for last_updated to make sure the most recent shows
-    // are saved in the DB.
-    showsList.forEach((show) => {
-      Show.findOne({ product_id: show.product_id }, (err, foundShow) => {
-        if (err) throw new Error({ message: 'Error finding show', error: err })
-        if (foundShow) {
-          return console.log('Show already exists')
-        }
-
-        if (!foundShow) {
-          const newShow = new Show({
-            product_id: show.product_id,
-            handle: show.handle,
-            updated_at: show.updated_at,
-            title: show.title,
-            variants: show.variants,
-            vendor: show.vendor,
-          }) 
-          console.log(`${show.product_id} saved!`)
-          return newShow.save()
-        }
-      })
-    })
+    dbService.saveShowsToDatabase(showsList)
 
     return res.status(200).json({ message: 'Shows updated' })
 
---
+++ services/db-service.js
@@ -0,0 +1,29 @@
+const Show = require('../models/show');
+
+exports.saveShowsToDatabase = (showsList) => {
+  showsList.forEach((show) => {
+    Show.findOne({ product_id: show.product_id }, (err, foundShow) => {
+      if (err) throw new Error({ message: 'Error finding show', error: err })
+
+      // if show already exists, skip it
+      if (foundShow) {
+        return console.log('Show already exists')
+      }
+
+      // if show doesn't exist in the DB, save it
+      if (!foundShow) {
+        const newShow = new Show({
+          product_id: show.product_id,
+          handle: show.handle,
+          updated_at: show.updated_at,
+          title: show.title,
+          variants: show.variants,
+          vendor: show.vendor,
+        }) 
+
+        console.log(`${show.product_id} saved!`)
+        return newShow.save()
+      }
+    })
+  })
+}

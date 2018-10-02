84635e64b67e06fa5b5d3e6d41a13258c03f4a72
Adam Bohannon <abo46n2@gmail.com>
2018-10-01 20:05:24 -0700

    More edits

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -7,37 +7,43 @@ import { Card, Table } from 'components/common';
 
 const OrdersTable = (props) => {
   const columns = [{
+    title: 'Order #',
+    dataIndex: 'orderNumber',
+  },
+  {
     title: 'Name',
     dataIndex: 'name',
-    render: text => <a href="#">{text}</a>,
-  }, {
-    title: 'Age',
-    dataIndex: 'age',
-  }, {
-    title: 'Address',
-    dataIndex: 'address',
-  }];
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+  },
+  ];
 
   const data = [{
     key: '1',
+    orderNumber: '#100',
     name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-  }, {
+    email: 'john@email.com',
+    status: 'Unsent',
+  },
+  {
     key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  }, {
+    orderNumber: '#101',
+    name: 'Ashley Smith',
+    email: 'ashley@email.com',
+    status: 'Unsent',
+  },
+  {
     key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-  }, {
-    key: '4',
-    name: 'Disabled User',
-    age: 99,
-    address: 'Sidney No. 1 Lake Park',
+    orderNumber: '#102',
+    name: 'Adam Bo',
+    email: 'adam@email.com',
+    status: 'Unsent',
   }];
 
   const rowSelection = {

e8bd210a7f503300cb83c48484176091344311a0
Adam Bohannon <abo46n2@gmail.com>
2018-10-02 14:38:32 -0700

    Added sendgrid and completed updateUser route with email notification

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/user';
+import { sendUpdatedUserEmail } from '../services/userService';
 
 export const updateUser = async (req, res) => {
   const userId = req.user.id;
@@ -6,14 +7,28 @@ export const updateUser = async (req, res) => {
 
   try {
     const updatedUser = await User.findOneAndUpdate({ _id: userId }, update);
-
     if (!updatedUser) {
       throw new Error('No user found.');
     }
 
+    if (updatedUser.email !== req.body.email) {
+      const options = {
+        newEmail: req.body.email,
+        name: req.body.name,
+      };
+
+      const emailResponse = await sendUpdatedUserEmail(options);
+
+      if (!emailResponse) {
+        throw new Error('Error sending email');
+      }
+
+      console.log(`Email sent to ${req.body.email}`);
+    }
+
     return res.status(200).send({ status: 200, message: 'Success' });
   } catch (err) {
-    res.status(501).send({ error: err.message });
+    return res.status(501).send({ error: err.message });
   }
 };
 
